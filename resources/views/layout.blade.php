728c583c189f466495e9affd9ec217c823297323
vagrant <forkarlmail@gmail.com>
2016-03-22 16:12:40 +0000

    added nalytics

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Youpornflix.com</title>
+    <title>Free Porn Tube Flix - Youpornflix.com</title>
 
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
     <!-- Latest compiled and minified CSS -->
@@ -174,5 +174,15 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-75449335-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 </body>
 </html>
