99df0f83fdfe8ac55c15ec31756e1c378b616a91
vagrant <forkarlmail@gmail.com>
2016-03-23 01:03:42 +0000

    new home page

--- app/Tag.php
+++ app/Tag.php
@@ -11,4 +11,16 @@ class Tag extends Model
     {
         return $this->belongsToMany('App\Scene','scene_has_tags');
     }
+
+    public function scopePrimaryThumbnails($query)
+	{
+		$thumbnail_id = $query->thumbnail_id;
+	    return $query->where('...')->get();
+	}
+
+	/* DVD count per tag */
+    public function getPrimaryThumbnailAttribute() {
+	    return Thumbnail::find($this->thumbnail_id)->url;
+    }
+
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,12 +1,17 @@
 @extends('layout')
 
 @section('content')
+
 <div class="content">
 	<h1 class="text-left">Popular Categories</h1>
     @foreach($tags as $tag)
     <div class="thumbs">
         <a href="/search/{{ str_slug($tag->name) }}" title="{{ $tag->name }}" alt="{{ $tag->name }}" target="_blank">
-            <img src="{{ $tag->scene()->orderBy('created_at', 'desc')->first()->primary_thumbnail }}" class="img-responsive center-block">
+        	@if($tag->thumbnail_id)
+        		<img src="{{ $tag->PrimaryThumbnail }}" class="img-responsive center-block">
+        	@else
+        		<img src="{{ $tag->scene()->orderBy('created_at','desc')->first()->primary_thumbnail }}" class="img-responsive center-block">
+        	@endif
         </a>
         <h5>{{ $tag->name }}</h5>
     </div>
