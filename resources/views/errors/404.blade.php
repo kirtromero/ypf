3b41846b8fdeaa1263b03bad37638d8952289d35
vagrant <forkarlmail@gmail.com>
2016-04-08 05:48:02 +0000

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+<div class="content">
+    <h1 class="center">Oops! Page not found. <small>Please try a different category below.</small></h1>
+</div>
+@stop
